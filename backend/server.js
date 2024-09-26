cb7aac79ada61d38293bc1f78588818810749663
Muniafu <elambaraka2019@gmail.com>
2024-09-26 07:26:33 +0300

    updated sever.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,24 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
+const auth = require('./routes/auth');
+const employee = require('./routes/employee');
+const feedback = require('./routes/feedback');
+const analytics = require('./routes/analytics');
+const manager = require('./routes/manager');
+
+dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-    .connect('mongodb://localhost:27017/employee-database', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
-
-// Define routes
-app.use('api/auth', require('./routes/auth'))
-app.use('/api/employees', require('./routes/employees'));
-app.use('/api/managers', require('./routes/managers'));
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-
-// Export the app
+// Routes
+app.use('/api/auth', auth );
+app.use('/api/employee', employee );
+app.use('/api/feedback', feedback );
+app.use('/api/analytics', analytics );
+app.use('/api/manager', manager );
